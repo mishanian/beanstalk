6c1273bad8958b818c947a4639cda79cc7f5d304
Mehran Ishanian <mishanian@gmail.com>
2025-03-16 02:06:48 -0400

    add!

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from Elastic Beanstalk with ESM!');
+    res.send('Hello from Elastic Beanstalk with ESM!!');
 });
 
 app.listen(port, () => {
